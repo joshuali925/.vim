4d612b01ac6c775d15515130f4d536c23e429ea1
Joshua Li <joshuali925@gmail.com>
2020-09-18 21:10:04 -0700

    update

---
+++ bin/lf-preview.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+unset COLORTERM
+
+case "$1" in
+    *.tar*) tar tf "$1";;
+    *.zip) zipinfo -1 "$1";;
+    *.rar) unrar l "$1";;
+    *.7z) 7z l "$1";;
+    # *.pdf) pdftotext "$1" -;;
+    *) bat --color=always --theme=OneHalfDark --plain --line-range :50 "$1";;
+esac
